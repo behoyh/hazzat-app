64de947b2095a009860ccb738ed5bf7893420d02
beshoy hanna <beshoyhanna@Beshoys-MacBook.local>
2017-11-10 17:05:14 -0500

    Navigation to section page works kinda

---
+++ Hazzat/Hazzat.iOS/DataSource.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Hazzat;
+using System.Collections.Generic;
+using Touch;
+
+namespace Hazzat.iOS
+{
+    public class DataSource : UITableViewSource
+    {
+        static readonly NSString CellIdentifier = new NSString("cell");
+        string[] objects;
+
+        public MasterViewController _controller;
+
+        public DataSource(string[] items, MasterViewController controller)
+        {
+            objects = items;
+            _controller = controller;
+        }
+        public IList<object> Objects
+        {
+            get { return objects; }
+        }
+
+        // Customize the number of sections in the table view.
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return objects.Length;
+        }
+
+        // Customize the appearance of table view cells.
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = (UITableViewCell)tableView.DequeueReusableCell(CellIdentifier, indexPath);
+
+            string item = objects[indexPath.Row];
+
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);
+            }
+
+            cell.TextLabel.Text = item;
+
+            return cell;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            SectionViewController newview = _controller.Storyboard.InstantiateViewController("SectionView") as SectionViewController;
+            if (newview != null)
+            {
+                _controller.NavigationController.PushViewController(newview, true);
+            }
+            tableView.DeselectRow(indexPath, true);
+        }
+
+        public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+        {
+            // Return false if you do not want the specified item to be editable.
+            return true;
+        }
+    }
+}
--- Hazzat/Hazzat.iOS/MasterViewController.cs
+++ Hazzat/Hazzat.iOS/MasterViewController.cs
@@ -5,6 +5,7 @@ using Hazzat.Service;
 using Foundation;
 using UIKit;
 using System.Threading.Tasks;
+using Hazzat.iOS;
 
 namespace Touch
 {
@@ -66,67 +67,6 @@ namespace Touch
                     TableView.Source = _dataSource = new DataSource(lst.ToArray(), this);
                 });
             });
-		}
-
-		public class DataSource : UITableViewSource
-		{
-			static readonly NSString CellIdentifier = new NSString ("cell");
-			string[] objects;
-
-            public MasterViewController _controller;
-
-            public DataSource(string[] items, MasterViewController controller)
-            {
-                objects = items;
-                _controller = controller;
-            }
-			public IList<object> Objects {
-                get { return objects; }
-            }
-
-			// Customize the number of sections in the table view.
-			public override nint NumberOfSections (UITableView tableView)
-			{
-				return 1;
-			}
-
-			public override nint RowsInSection (UITableView tableview, nint section)
-			{
-                return objects.Length;
-			}
-
-            // Customize the appearance of table view cells.
-            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-            {
-                var cell = (UITableViewCell)tableView.DequeueReusableCell(CellIdentifier, indexPath);
-
-                string item = objects[indexPath.Row];
-
-                if (cell == null)
-                { 
-                    cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); 
-                }
-
-                cell.TextLabel.Text = item;
-
-                return cell;
-            }
-
-            public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
-            {
-                UIAlertController okAlertController = UIAlertController.Create("Row Selected", objects[indexPath.Row], UIAlertControllerStyle.Alert);
-                okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
-    
-                _controller.PresentViewController(okAlertController, true, null);
-
-                tableView.DeselectRow(indexPath, true);
-            }
-
-			public override bool CanEditRow (UITableView tableView, NSIndexPath indexPath)
-			{
-				// Return false if you do not want the specified item to be editable.
-				return true;
-			}
 		}
 	}
 }
