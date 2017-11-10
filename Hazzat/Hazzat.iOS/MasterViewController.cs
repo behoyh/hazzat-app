7ce0497d6864c26dda7939f144c44e7d0cc0fe55
beshoy hanna <beshoyhanna@Beshoys-MacBook.local>
2017-11-10 15:41:48 -0500

    Add touch events

--- Hazzat/Hazzat.iOS/MasterViewController.cs
+++ Hazzat/Hazzat.iOS/MasterViewController.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 using Hazzat.Service;
 using Foundation;
 using UIKit;
+using System.Threading.Tasks;
 
 namespace Touch
 {
 	public partial class MasterViewController : UITableViewController
 	{
-		DataSource _dataSource;
-        HazzatController _controller;
+		private DataSource _dataSource;
+        private HazzatController _controller;
+
 		public MasterViewController (IntPtr handle) : base (handle)
 		{
 			Title = NSBundle.MainBundle.LocalizedString("Menu", "Main Menu");
-
-			// Custom initialization
 		}
 
 		void AddNewItem (object sender, EventArgs args)
@@ -46,21 +46,13 @@ namespace Touch
         {
             base.ViewDidLoad();
 
+            _controller = new HazzatController();
 
             NavigationItem.RightBarButtonItem = new UIBarButtonItem(UIImage.FromFile("settings.png")
                 ,UIBarButtonItemStyle.Plain 
                 ,(ooo,oo)=>{
                     
                 });
-
-
-            UIBarButtonItem settingsButton = new UIBarButtonItem();
-
-			NavigationItem.RightBarButtonItem = settingsButton;
-
-			
-
-            _controller = new HazzatController();
 
             List<string> lst = new List<string>();
             _controller.GetSeasons(true, (evt, data) =>
@@ -71,19 +63,22 @@ namespace Touch
                 }
                 InvokeOnMainThread(() =>
                 {
-                    TableView.Source = _dataSource = new DataSource(lst.ToArray());
+                    TableView.Source = _dataSource = new DataSource(lst.ToArray(), this);
                 });
             });
 		}
 
-		class DataSource : UITableViewSource
+		public class DataSource : UITableViewSource
 		{
 			static readonly NSString CellIdentifier = new NSString ("cell");
 			string[] objects;
 
-            public DataSource(string[] items)
+            public MasterViewController _controller;
+
+            public DataSource(string[] items, MasterViewController controller)
             {
                 objects = items;
+                _controller = controller;
             }
 			public IList<object> Objects {
                 get { return objects; }
@@ -115,6 +110,16 @@ namespace Touch
                 cell.TextLabel.Text = item;
 
                 return cell;
+            }
+
+            public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+            {
+                UIAlertController okAlertController = UIAlertController.Create("Row Selected", objects[indexPath.Row], UIAlertControllerStyle.Alert);
+                okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+    
+                _controller.PresentViewController(okAlertController, true, null);
+
+                tableView.DeselectRow(indexPath, true);
             }
 
 			public override bool CanEditRow (UITableView tableView, NSIndexPath indexPath)
